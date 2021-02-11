ec2490fbbbb405d83daf9ee5275696f6b214ed8f
Alexander Skiba <alex@aeriform.one>
2021-02-11 21:33:28 +0700

    [Compmath] Refactor both integration solutions in A2
    Use scientific format for errors, accept user input for integration error

--- compmath/assignment_2/gauss_legendre_quadrature.rb
+++ compmath/assignment_2/gauss_legendre_quadrature.rb
@@ -1,5 +1,10 @@
 require "bigdecimal/math"
 
+def get_big_decimal(desc)
+  $> << "Set the #{desc} (*required, decimal): "
+  BigDecimal(gets).tap { |val| puts "#{desc.capitalize} set to #{val}" }
+end
+
 negative_half = [
   [BigDecimal("-0.932469515"), BigDecimal("0.17132450")],
   [BigDecimal("-0.66120939"), BigDecimal("0.36076158")],
@@ -9,12 +14,12 @@ negative_half = [
 QUADRATURE_6 = negative_half + negative_half.reverse.map { |t, w| [-t, w] }
 
 def partitioned_gauss_integral(f, a, b, j)
-  sum = QUADRATURE_6.map do |t, w|
-    w * (0..j).map do |i|
+  sum = QUADRATURE_6.sum do |t, w|
+    w * (0..j).sum do |i|
       root = (b + a * (2 * j + 1)) / (2 * (j + 1)) + (b - a) / (2 * (j + 1)) * t
       f[root + (b - a) / (j + 1) * i]
-    end.sum
-  end.sum
+    end
+  end
 
   (b - a) / (2 * (j + 1)) * sum
 end
@@ -31,13 +36,13 @@ def gauss_integral(f, a, b, delta)
   { integral: s, delta: actual_delta }
 end
 
+puts "Definite integral approximation with Gauss-Legendre quadrature"
+
 f = ->(x) { (1 + x ** 3).sqrt(20) }
 a = BigDecimal(0)
 b = BigDecimal(2)
-error = BigDecimal("1e-10")
-
+error = get_big_decimal("error")
 result = gauss_integral(f, a, b, error)
 
-puts "Definite integral approximation with Gauss-Legendre quadrature"
-puts "integral = #{"%.15f" % result[:integral]}"
-puts "error = #{"%.15f" % result[:delta]}"
+puts "integral = #{"%g" % result[:integral]}"
+puts "error = #{"%e" % result[:delta]}"
--- compmath/assignment_2/midpoint_integration.rb
+++ compmath/assignment_2/midpoint_integration.rb
@@ -1,10 +1,17 @@
 require "bigdecimal/math"
 
+def get_big_decimal(desc)
+  $> << "Set the #{desc} (*required, decimal): "
+  BigDecimal(gets).tap { |val| puts "#{desc.capitalize} set to #{val}" }
+end
+
+puts "Numerical integration with midpoint rule:"
+
 f = ->(x) { (1 + x ** 3).sqrt(20) }
 d2f = ->(x) { 3 * x * (x.power(3, 20) + 4) / (4 * (1 + x.power(3, 20)).power(3 / 2r, 20)) }
 a = BigDecimal(0)
 b = BigDecimal(2)
-error = BigDecimal("1e-5")
+error = get_big_decimal("error")
 
 m2 = [d2f[a], d2f[b]].max
 n = (m2 * (b - a).power(3, 20) / (24 * error)).sqrt(20).ceil
@@ -12,7 +19,6 @@ h = (b - a) / n
 integral = n.times.map { |i| f[a + h * (i + 1 / 2r)] }.sum * h
 res_error = (b - a) / 24 * h ** 2 * m2
 
-puts "Numerical integration with midpoint rule:"
-puts "integral = #{integral.to_f}"
+puts "integral = #{"%g" % integral}"
 puts "n = #{n}"
-puts "error = #{"%.15f" % res_error}"
+puts "error = #{"%e" % res_error}"
