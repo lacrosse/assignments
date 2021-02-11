572ec9e0d968f88aaafd82509f6167b96857afd0
Alexander Skiba <alex@aeriform.one>
2021-02-11 21:06:34 +0700

    [Compmath] Refactor differentiation in A2
    Use scientific format for errors

--- compmath/assignment_2/differentiation.rb
+++ compmath/assignment_2/differentiation.rb
@@ -15,22 +15,20 @@ class LagrangePolynomial
   def derivative(n, x)
     range = (0...order).to_a
 
-    range.map do |i|
+    range.sum do |i|
       subrange = range - [i]
       numerator =
         factorial(n) *
         subrange
           .map { |j| x - @xs[j] }
           .combination(subrange.count - n)
-          .map { |c| c.reduce(:*) }
-          .sum
+          .sum { |c| c.reduce(:*) }
       denominator =
         subrange
-          .map { |j| @xs[i] - @xs[j] }
-          .reduce(:*)
+          .reduce(1) { |p, j| p * (@xs[i] - @xs[j]) }
 
       @ys[i] * numerator / denominator
-    end.sum
+    end
   end
 
   def derivative_error(n, x)
@@ -39,8 +37,7 @@ class LagrangePolynomial
       @xs
         .map { |x_i| x - x_i }
         .combination(@xs.count - n)
-        .map { |c| c.reduce(:*) }
-        .sum
+        .sum { |c| c.reduce(:*) }
 
     div_diffs(@xs.zip(@ys)) * sum_of_prods.abs
   end
@@ -58,6 +55,14 @@ class LagrangePolynomial
       div_diffs(tuples[1..-1]) - div_diffs(tuples[0..-2]) / (tuples[-1][0] - tuples[0][0])
     end
   end
+end
+
+def inspect_number(n)
+  "%g" % n
+end
+
+def inspect_error(n)
+  "%e" % n
 end
 
 y = ->(x) { 1 / (BigMath.sin(x, 100) + BigMath.cos(x, 100)) }
@@ -80,7 +85,7 @@ result =
 
 puts "Lagrange derivatives:"
 result.each do |x, d1, ed1, d2, ed2|
-  puts ". x = #{x.to_f}"
-  puts "  dy/dx = #{d1.to_f} (error = #{ed1.to_f})"
-  puts "  d2y/dx2 = #{d2.to_f} (error = #{ed2.to_f})"
+  puts ". x = #{inspect_number(x)}"
+  puts "  dy/dx = #{inspect_number(d1)}  (error = #{inspect_error(ed1)})"
+  puts "  d2y/dx2 = #{inspect_number(d2)}  (error = #{inspect_error(ed2)})"
 end
