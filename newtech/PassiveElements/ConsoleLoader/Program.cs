fa1b8e17fa710c2e959c8421e111cfcf52c6b816
Alexander Skiba <alex@aeriform.one>
2021-02-14 06:43:30 +0700

    [Newtech] Add A1 code: passive component classes, impedance functions

---
+++ newtech/PassiveElements/ConsoleLoader/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLoader
+{
+    class Program
+    {
+        static double readDouble()
+        {
+            string str;
+            
+            while (true)
+            {
+                str = Console.ReadLine();
+                try
+                {
+                    return Double.Parse(str);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"I can't parse {str}. Try again.");
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Model.PassiveComponent component;
+
+            Console.WriteLine("Good morning, Dave.");
+            Console.WriteLine("Today is our component testing day.");
+            Console.WriteLine("We'll be creating several components and viewing their impedance (aka complex resistance).");
+            Console.WriteLine("Impedance is a function Z(jω), so I'll be asking you for the arguments as well.");
+
+            Console.WriteLine("Give me a value, I'll assume it's ohms and create a resistor for you:");
+
+            component = new Model.Resistor(readDouble());
+
+            Console.WriteLine($"Here's your component: {component}.");
+            Console.WriteLine($"Resistor's impedance is constant and equals {component.Impedance(0)} ohm for any jω. It's so great that we have Unicode.");
+            Console.WriteLine("Moving on. Give me farads and I'll create a capacitor:");
+
+            component = new Model.Capacitor(readDouble());
+
+            Console.WriteLine($"Here's your component: {component}. Now give me a jω argument for the impedance function:");
+
+            double var = readDouble();
+
+            Console.WriteLine($"Capacitor's impedance equals {component.Impedance(var)} ohm at {var}.");
+            Console.WriteLine("One more. Give me henries and I'll create an inductor:");
+
+            component = new Model.Inductor(readDouble());
+
+            Console.WriteLine($"Here's your component: {component}. Now give me a jω argument for the impedance function:");
+
+            var = readDouble();
+
+            Console.WriteLine($"Inductor's impedance equals {component.Impedance(var)} ohm at {var}.");
+            Console.WriteLine("Thank you, Dave. See you later. Press any key to put me to sleep.");
+
+            Console.Read();
+        }
+    }
+}
---
+++ newtech/PassiveElements/Model/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public abstract class PassiveComponent
+    {
+        public PassiveComponent(double var)
+        {
+            ComponentVariable = var;
+        }
+
+        public abstract string Name { get; }
+        public abstract string ShortName { get; }
+        public abstract string VarName { get; }
+
+        private double _component_variable;
+        public double ComponentVariable
+        {
+            get => _component_variable;
+            set
+            {
+                if (_component_variable < 0)
+                    throw new ArgumentException($"You can't have a {Name} with negative {VarName}.");
+                else
+                    _component_variable = value;
+            }
+        }
+
+        public override string ToString() => $"[#{ShortName} = {ComponentVariable}]";
+        public abstract double Impedance(double arg);
+    }
+
+    public class Resistor : PassiveComponent
+    {
+        public Resistor(double resistance) : base(resistance) { }
+
+        public override string Name { get => "resistor"; }
+        public override string ShortName { get => "R"; }
+        public override string VarName { get => "resistance";  }
+
+        public override double Impedance(double _arg) => ComponentVariable;
+    }
+
+    public class Capacitor : PassiveComponent
+    {
+        public Capacitor(double capacitance) : base(capacitance) { }
+
+        public override string Name { get => "capacitor"; }
+        public override string ShortName { get => "C"; }
+        public override string VarName { get => "capacitance"; }
+
+        public override double Impedance(double arg) => 1 / (arg * ComponentVariable);
+    }
+
+    public class Inductor : PassiveComponent
+    {
+        public Inductor(double inductance) : base(inductance) { }
+
+        public override string Name { get => "inductor"; }
+        public override string ShortName { get => "L"; }
+        public override string VarName { get => "inductance"; }
+
+        public override double Impedance(double arg) => arg * ComponentVariable;
+    }
+}
